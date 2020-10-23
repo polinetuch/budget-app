90dfcea50d4302fff4ef50b7107ddbcf2dd63fe2
Polinet <uchpolinet@gmail.com>
2020-10-23 14:52:20 +1100

    added formatNumber method

--- app.js
+++ app.js
@@ -143,6 +143,24 @@ var UIController = (function() {
         container: '.container',
         expensePercLabel: '.item__percentage'
     };
+
+    var formatNumber = function(num, type) {
+        var numSplit; 
+
+        num = Math.abs(num);
+        num = num.toFixed(2);
+
+        numSplit = num.split('.');
+
+        int = numSplit[0];
+        if (int.length > 3) {
+            int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3)
+        }
+
+        dec = numSplit[1];
+
+        return (type === 'exp' ? sign = '-' : sign = '+') + ' ' + int + '.' +  dec;
+    };
         return {
             getInput: function() {
                 return {
@@ -183,7 +201,7 @@ var UIController = (function() {
                 // Replace the placeholder text with some actual data
                 newHtml = html.replace('%id%', obj.id);
                 newHtml = newHtml.replace('%description%', obj.description);
-                newHtml = newHtml.replace('%value%', obj.value)
+                newHtml = newHtml.replace('%value%', formatNumber(obj.value, type))
 
                 // Insert HTML into the DOM
                 document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
@@ -240,7 +258,7 @@ var UIController = (function() {
 
             },
 
-               getDOMString: function() {
+            getDOMString: function() {
                 return DOMString;
             }
         }
